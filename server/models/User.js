3c5d5da8e223fb96b09d43f54176d24b7d6cd79d
Tinku Bansal <tinkubansal21@gmail.com>
2021-08-13 10:10:47 +1000

    Resolving errors

--- server/models/User.js
+++ server/models/User.js
@@ -36,16 +36,24 @@ const userSchema = new Schema({
 
 // set up pre-save middleware to create password
 userSchema.pre("save", async function (next) {
-  if (this.isNew || this.isModified("password")) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
+  try {
+    if (this.isNew || this.isModified("password")) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+  } catch (err) {
+    console.log(err);
   }
-  next();
 });
 
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const User = mongoose.model("User", userSchema);
